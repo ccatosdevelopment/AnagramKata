e34b909c408ad82f2cf79a2b1f7b48a7bdd93ad9
ccatosdevelopment <110388345+ccatosdevelopment@users.noreply.github.com>
2022-10-11 17:26:50 +0100

    Implement first solution for anagram solving

--- src/main/scala/com/app/example/Main.scala
+++ src/main/scala/com/app/example/Main.scala
@@ -4,52 +4,64 @@ import scala.io.{BufferedSource, Source}
 
 object Main {
   def main(args: Array[String]): Unit = {
+
     // (1) Read in wordlist.txt and store all words in a Vector[String]
     val wordListFilepath: String = "src/main/resources/word_list.txt"
-    val wordListSource: BufferedSource = Source.fromFile(wordListFilepath)
+    val wordListFile: BufferedSource = Source.fromFile(wordListFilepath)
 
-    val readWordsFromFile: () => Vector[String] = () => {
+    val readWordsFromFile = (file: BufferedSource) => {
       for {
-        linesInFile <- wordListSource.getLines().toVector
+        linesInFile <- file.getLines().toVector
         wordsInLine <- linesInFile.split(" +")
       } yield wordsInLine
     }
 
-    val words: Vector[String] = readWordsFromFile()
-//    for (word <- words) println(word)
+    val allWords: Vector[String] = readWordsFromFile(wordListFile)
 
+//--------------------------------------------------------------------------------------------------------------------//
 
-    // TODO: (2) Filter out all invalid words
+    // (2) Filter potential valid words:
+    val inputAnagram: String = "documenting"
 
+    val filterPotentialValidWords = (wordList: Vector[String], fullAnagram: String) => {
+      for (word <- wordList if word.intersect(fullAnagram).equals(word)) yield word
+    }
 
+    val validWords: Vector[String] = filterPotentialValidWords(allWords, inputAnagram)
 
+//--------------------------------------------------------------------------------------------------------------------//
 
+    // (3) Check all possible combinations of potential valid words
+    val determineAnagrams = (wordList: Vector[String], fullAnagram: String) => {
+      for {
+        word <- wordList
+        comparisonWord <- wordList.slice(wordList.indexOf(word) + 1, wordList.size)
+        if fullAnagram.diff(word).sorted.equals(comparisonWord.sorted)
+      } yield (word, comparisonWord)
+    }
 
+    val validAnagrams: Vector[(String, String)] = determineAnagrams(validWords, inputAnagram)
 
+//--------------------------------------------------------------------------------------------------------------------//
 
+    // (4) Output all valid word combinations
+    printf("Word to solve anagrams for: \"%s\"\n", inputAnagram)
+    printf("Word list to generate solutions from: \"%s\"\n", wordListFilepath)
+    printf("Calculating valid 2-word anagrams: \n")
+    for (tuple <- validAnagrams) printf("-> Solution found: (\"%s\", \"%s\")\n", tuple._1, tuple._2)
+    printf("End of solutions")
 
-
-
-
-
-
-
-
-    // TODO: (3) Check all possible combinations of remaining words
-
-
-    // TODO: (4) Output all valid word combinations
-
+//--------------------------------------------------------------------------------------------------------------------//
 
     // (5) Close any open resources
-      wordListSource.close
+      wordListFile.close
+
+//--------------------------------------------------------------------------------------------------------------------//
 
     // Potential improvements:
     // Improve the performance (e.g. using word length to filter invalid combinations quicker?)
     // Implement a Try/Success/Failure idiom for file handling (both opening and reading)
-    // Could Regex improve things here? Read in word list and apply Regex to filter out invalid words?
 
+    //TODO: Remove all comments to self!
   }
 }
-
-//TODO: Remove all comments to self!
