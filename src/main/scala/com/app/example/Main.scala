a03c55fa5a20c24c1224a8a394908518e8d6e226
ccatosdevelopment <110388345+ccatosdevelopment@users.noreply.github.com>
2022-10-11 17:37:01 +0100

    Make minor changes to improve readability

--- src/main/scala/com/app/example/Main.scala
+++ src/main/scala/com/app/example/Main.scala
@@ -5,6 +5,7 @@ import scala.io.{BufferedSource, Source}
 object Main {
   def main(args: Array[String]): Unit = {
 
+    val inputAnagram: String = "documenting"
     val wordListFilepath: String = "src/main/resources/word_list.txt"
     val wordListFile: BufferedSource = Source.fromFile(wordListFilepath)
 
@@ -18,15 +19,11 @@ object Main {
     val allWords: Vector[String] = readWordsFromFile(wordListFile)
 
 
-
-    val inputAnagram: String = "documenting"
-
     val filterPotentialValidWords = (wordList: Vector[String], fullAnagram: String) => {
       for (word <- wordList if word.intersect(fullAnagram).equals(word)) yield word
     }
 
     val validWords: Vector[String] = filterPotentialValidWords(allWords, inputAnagram)
-
 
 
     val determineAnagrams = (wordList: Vector[String], fullAnagram: String) => {
@@ -40,14 +37,11 @@ object Main {
     val validAnagrams: Vector[(String, String)] = determineAnagrams(validWords, inputAnagram)
 
 
-
     printf("Word to solve anagrams for: \"%s\"\n", inputAnagram)
     printf("Word list to generate solutions from: \"%s\"\n", wordListFilepath)
     printf("Calculating valid 2-word anagrams: \n")
     for (tuple <- validAnagrams) printf("-> Solution found: (\"%s\", \"%s\")\n", tuple._1, tuple._2)
     printf("End of solutions")
-
-
 
     wordListFile.close
   }
