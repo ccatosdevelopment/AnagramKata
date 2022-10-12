57533c333bf518373d23aa444faf6624b9372368
ccatosdevelopment <110388345+ccatosdevelopment@users.noreply.github.com>
2022-10-12 15:55:58 +0100

    Extract methods from main

--- src/main/scala/com/app/example/Main.scala
+++ src/main/scala/com/app/example/Main.scala
@@ -3,39 +3,37 @@ package com.app.example
 import scala.io.{BufferedSource, Source}
 
 object Main {
+
+  def readWordsFromFile: BufferedSource => Vector[String] = (file: BufferedSource) => {
+    for {
+      linesInFile <- file.getLines().toVector
+      wordsInLine <- linesInFile.split(" +")
+    } yield wordsInLine
+  }
+
+  def filterPotentialValidWords: (Vector[String], String) => Vector[String]  = (wordList: Vector[String], fullAnagram: String) => {
+    for (word <- wordList if word.intersect(fullAnagram).equals(word)) yield word
+  }
+
+  def determineAnagrams: (Vector[String], String) => Vector[(String, String)] = (wordList: Vector[String], fullAnagram: String) => {
+    for {
+      word <- wordList
+      comparisonWord <- wordList.slice(wordList.indexOf(word) + 1, wordList.size)
+      if fullAnagram.diff(word).sorted.equals(comparisonWord.sorted)
+    } yield (word, comparisonWord)
+  }
+
   def main(args: Array[String]): Unit = {
 
     val inputAnagram: String = "documenting"
     val wordListFilepath: String = "src/main/resources/word_list.txt"
     val wordListFile: BufferedSource = Source.fromFile(wordListFilepath)
 
-    val readWordsFromFile = (file: BufferedSource) => {
-      for {
-        linesInFile <- file.getLines().toVector
-        wordsInLine <- linesInFile.split(" +")
-      } yield wordsInLine
-    }
-
     val allWords: Vector[String] = readWordsFromFile(wordListFile)
-
-
-    val filterPotentialValidWords = (wordList: Vector[String], fullAnagram: String) => {
-      for (word <- wordList if word.intersect(fullAnagram).equals(word)) yield word
-    }
 
     val validWords: Vector[String] = filterPotentialValidWords(allWords, inputAnagram)
 
-
-    val determineAnagrams = (wordList: Vector[String], fullAnagram: String) => {
-      for {
-        word <- wordList
-        comparisonWord <- wordList.slice(wordList.indexOf(word) + 1, wordList.size)
-        if fullAnagram.diff(word).sorted.equals(comparisonWord.sorted)
-      } yield (word, comparisonWord)
-    }
-
     val validAnagrams: Vector[(String, String)] = determineAnagrams(validWords, inputAnagram)
-
 
     printf("Word to solve anagrams for: \"%s\"\n", inputAnagram)
     printf("Word list to generate solutions from: \"%s\"\n", wordListFilepath)
